e662ca97f6789d41a9e72342c8a512a442e4289f
Sudhanshu pandit <sudhanshuk1140@gmail.com>
2023-10-23 21:51:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Button, Popover, Typography, TextField, MenuItem } from '@mui/material';
 import './App.css';
 
+const citationStyles = [
+  { value: 'apa', label: 'APA' },
+  { value: 'mla', label: 'MLA' },
+  { value: 'chicago', label: 'Chicago' },
+  { value: 'ieee', label: 'IEEE' },
+  { value: 'harvard', label: 'Harvard' },
+];
+
 function App() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [citationStyle, setCitationStyle] = useState('');
+  const [inputText, setInputText] = useState('');
+  const [citationText, setCitationText] = useState('');
+
+  const handleOpenPopup = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClosePopup = () => {
+    setAnchorEl(null);
+  };
+
+  const handleGenerateCitation = () => {
+    const generatedCitation = `Generated citation in ${citationStyle} style: ${inputText}`;
+    setCitationText(generatedCitation);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+
+
+      <div className="popup-container">
+        <Button onClick={handleOpenPopup}>Citations</Button>
+        <Popover
+          open={Boolean(anchorEl)}
+          anchorEl={anchorEl}
+          onClose={handleClosePopup}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'bottom',
+          }}
+          transformOrigin={{
+            vertical: 'bottom',
+            horizontal: 'bottom',
+          }}
         >
-          Learn React
-        </a>
-      </header>
+
+          <div className="popup-content">
+
+            <Typography variant="h6">Generate Citations</Typography>
+            <TextField
+              select
+              label="Citation Style"
+              value={citationStyle}
+              onChange={(e) => setCitationStyle(e.target.value)}
+              fullWidth
+            >
+              {citationStyles.map((style) => (
+                <MenuItem key={style.value} value={style.value}>
+                  {style.label}
+                </MenuItem>
+              ))}
+
+
+            </TextField>
+
+            <Button onClick={handleGenerateCitation} variant="contained" color="primary">
+              Generate Citation
+            </Button>
+
+            {citationText && (
+              <Typography variant="body1" className="citation-text">
+                {citationText}
+              </Typography>
+
+              
+            )}
+          </div>
+        </Popover>
+      </div>
+
+      <div className="input-container">
+        <TextField
+          label="Input Text"
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+          fullWidth
+          multiline
+          rows={4}
+        />
+      </div>
     </div>
   );
 }
